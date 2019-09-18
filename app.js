5064cc183e54b123a91f3ccc9f6cf70237d5fe2a
Romane G <romane71193@gmail.com>
2019-09-17 20:56:34 -0400

    in an express app

---
+++ app.js
@@ -0,0 +1,91 @@
+//var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+import uuidv4 from 'uuid/v4';
+//var indexRouter = require('./routes/index');
+//var usersRouter = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+//app.set('views', path.join(__dirname, 'views'));
+//app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+//app.use(express.static(path.join(__dirname, 'public')));
+
+//app.use('/', indexRouter);
+//app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+// app.use(function(req, res, next) {
+//   next(createError(404));
+// });
+
+// error handler
+// app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  // res.locals.message = err.message;
+  // res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+//   res.status(err.status || 500);
+//   res.render('error');
+// });
+
+let users = {
+  1: {
+    id: '1',
+    username: 'Robin Wieruch',
+  },
+  2: {
+    id: '2',
+    username: 'Dave Davids',
+  },
+};
+let messages = {
+  1: {
+    id: '1',
+    text: 'Hello World',
+    userId: '1',
+  },
+  2: {
+    id: '2',
+    text: 'By World',
+    userId: '2',
+  },
+};
+
+
+app.get('/', (req, res) => {
+  return res.send('Received a GET HTTP method');
+});
+app.post('/', (req, res) => {
+  return res.send('Received a POST HTTP method');
+});
+app.put('/', (req, res) => {
+  return res.send('Received a PUT HTTP method');
+});
+app.delete('/', (req, res) => {
+  return res.send('Received a DELETE HTTP method');
+}); 
+
+
+app.get('/users', (req, res) => {
+  return res.send('GET HTTP method on user resource');
+});
+app.post('/users', (req, res) => {
+  return res.send('POST HTTP method on user resource');
+});
+app.put('/users/:userId', (req, res) => {
+  return res.send('PUT HTTP method on user resource');
+});
+app.delete('/users/:userId', (req, res) => {
+  return res.send('DELETE HTTP method on user resource');
+});
+module.exports = app;
